3f27f18dfb13c169aabef4bc4ba9d2387d2e8e36
Dancs Gergely <dancs.gergely@flexoon.hu>
2024-03-02 15:11:11 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TodosStore} from "./store/todos.store";
+import {JsonPipe} from "@angular/common";
+import {TodoListComponent} from "./todo-list/todo-list.component";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe, TodoListComponent, MatProgressSpinner],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ngrx-signal-store';
+export class AppComponent implements OnInit{
+
+  store = inject(TodosStore);
+
+  ngOnInit(): void {
+    this.loadTodos()
+      .then(()=> console.log("todos loaded"));
+  }
+
+  async loadTodos(){
+    await this.store.loadAll();
+  }
+
 }
---
+++ src/app/model/mock-data.ts
@@ -0,0 +1,9 @@
+import {Todo} from "./todo.model";
+
+export let TODOS: Todo[] = [
+  { id: "1", title: 'Task 1', completed: false },
+  { id: "2", title: 'Task 2', completed: false },
+  { id: "3", title: 'Task 3', completed: false },
+  { id: "4", title: 'Task 4', completed: false },
+  { id: "5", title: 'Task 5', completed: false },
+]
